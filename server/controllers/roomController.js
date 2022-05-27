74828d3d14bf7d2e56f3dce77d462aa9fe6962d4
SunDust23 <julik23@bk.ru>
2022-05-27 17:25:22 +0500

    Update roomController.js
    Create and delete room

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -2,10 +2,13 @@ const { Room } = require('../models/models');
 
 class RoomController {
     async create(req, res) {
-
+        const {num, subject} = req.body;
+        const room = await Room.create({num, subject});
+        return res.json(room);
     }
     async getAll(req, res) {
-        
+        const rooms = await Room.findAll();
+        return res.json(rooms);
     }
 }
 module.exports = new RoomController();
