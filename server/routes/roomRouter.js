5fe4c6940672b0582ad2351301a334cdfc278154
SunDust23 <julik23@bk.ru>
2022-05-29 23:21:01 +0500

    Update routes
    Added check role middleware to routes

--- server/routes/roomRouter.js
+++ server/routes/roomRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const router = new Router();
+const checkRole = require('../middleware/checkRoleMiddleware');
 
 const roomController = require('../controllers/roomController');
 
-router.post('/', roomController.create);
+router.post('/', checkRole("ADMIN"), roomController.create);
 router.get('/', roomController.getAll);
 
 module.exports = router;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,9 +3,16 @@ const userController = require('../controllers/userController');
 const router = new Router();
 
 const authMiddleware = require('../middleware/authMiddleware');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
+//Auth
 router.post('/registration', userController.registration);
 router.post('/login', userController.login);
 router.get('/auth', authMiddleware, userController.check);
+
+//CRUD for admins
+router.get('/', checkRole("ADMIN"), userController.getAll);
+router.put('/', checkRole("ADMIN"), userController.update);
+router.delete('/:id', checkRole("ADMIN"), userController.delOne);
 
 module.exports = router;
