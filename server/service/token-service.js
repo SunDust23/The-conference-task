8fd3b9559b7edf2d765e7354cd23094a6b48fb2e
SunDust23 <julik23@bk.ru>
2022-05-31 21:48:42 +0500

    Validate token

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -12,6 +12,24 @@ class TokenService {
         }
     }
 
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
     async saveToken(userId, refreshToken) {
         const tokenData = await Token.findOne({ where: { userId } });
         if (tokenData) {
@@ -23,7 +41,12 @@ class TokenService {
     }
 
     async removeToken(refreshToken) {
-        const tokenData = await Token.destroy({where: {refreshToken}});
+        const tokenData = await Token.destroy({ where: { refreshToken } });
+        return tokenData;
+    }
+
+    async findToken(refreshToken) {
+        const tokenData = await Token.findOne({ where: { refreshToken } });
         return tokenData;
     }
 }
